746f759103bbbf19ce89612f1a5e4c574107c0ab
Massimo <massimo.luppi@gmail.com>
2023-04-14 17:48:09 +0200

    Final commit

--- exp_web.py
+++ exp_web.py
@@ -3,19 +3,14 @@ import functions
 
 todos = functions.get_todos()
 
-st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
-
 st.title("My Todo App")
 st.subheader("Massimo's To Do App")
-st.write("App to improve <b>productivity</b>.", unsafe_allow_html=True)
-
-st.text_input(label=" ", placeholder="Add a new todo",
-              on_change=add_todo, key='new_todo')
+st.write("App to improve productivity.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -26,3 +21,5 @@ for index, todo in enumerate(todos):
         st._rerun()
 
 
+st.text_input(label=" ", placeholder="Add a new todo",
+              on_change=add_todo, key='new_todo')
